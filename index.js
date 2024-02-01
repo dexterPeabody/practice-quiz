b80c5628671b246b2d9c8f2ceb6802cb8a499a4e
dexterPeabody <158376561+dexterPeabody@users.noreply.github.com>
2024-02-01 16:03:52 -0500

    html and questions
    created html plate and wrote questioins and answers

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+Are you a bird or a cat
+
+what's your choice of dinner?
+	fish stew? CAT
+	chocolote worms? BIRD
+	fresh garden salad? BUNNY
+
+what's your choice of a comphy bed?
+	twisted mess of your own creation? BIRD
+	your boyfriends laundy? CAT
+	a warm burrow under the sheets? BUNNY
+
+what's your choice of toy?
+	something you can chase around? CAT
+	something you can pull and tug at? BIRD
+	something you can munch of? BUNNY
+
+what's the best means of travel? 
+	Private chauffeur though the cuntry side? CAT
+	A direct first class flight? BIRD
+	Train hopping cross cuntry? BUNNY
+
+what's the best part of the day?
+	A warm nap in the window? CAT
+	A cup of coffee at the break of dawn? BIRD
+	When the cool grass touches the toes? BUNNY */
