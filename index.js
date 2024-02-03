1a814fae3bae0ddf8c22f747f7e03cc42e068993
dexterPeabody <158376561+dexterPeabody@users.noreply.github.com>
2024-02-02 21:23:07 -0500

    added js
    got first js written. working submit button next to populate next question and verify that question was answered before proceeding.

--- index.js
+++ index.js
@@ -25,3 +25,67 @@ what's the best part of the day?
 	A warm nap in the window? CAT
 	A cup of coffee at the break of dawn? BIRD
 	When the cool grass touches the toes? BUNNY */
+let questionContainer = document.getElementById('question-container');
+let answerOne = document.getElementById('option-one-text');
+let answerTwo = document.getElementById('option-two-text');
+let answerThree = document.getElementById('option-three-text');
+
+let userScore = [];
+let currentQuestion = 0
+
+	
+let theFiveQuestions = [
+	{
+		question:"What is your choice of dinner?",
+		answers: [
+			"Fish stew",
+			"Chocolote worms",
+			"A fresh garden salad"
+		]
+	},
+	{
+		question: "What is your choice of a comphy bed?",
+		answers: [
+			"Your significant other's laundy",
+			"A twisted mess of your own creation",
+			"A warm burrow under the sheets"
+		]
+	},
+	{
+		question: "What is your choice of toy?",
+		answers: [
+			"Something you can chase around",
+			"Something you can pull and tug at",
+			"Something you can munch on"
+		]
+	},
+	{
+		question: "What is the best means of travel?",
+		answers: [
+			"A private chauffeur though the cuntry side",
+			"A direct first class flight",
+			"Train hopping cross cuntry"
+		]
+	},
+	{
+		question: "What is the best part of the day?",
+		answers: [
+			"A warm nap in the window",
+			"A cup of coffee at the break of dawn",
+			"When the cool grass touches the toes"
+		]
+	}
+]
+
+function runQuiz(){
+	questionContainer.innerHTML = theFiveQuestions[currentQuestion].question;
+	answerOne.innerHTML = theFiveQuestions[currentQuestion].answers[0];
+	answerTwo.innerHTML = theFiveQuestions[currentQuestion].answers[1];
+	answerThree.innerHTML = theFiveQuestions[currentQuestion].answers[2];
+}
+
+runQuiz()
+
+function submit-btn(){
+
+}
